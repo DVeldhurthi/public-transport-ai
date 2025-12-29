00e05f36540cd5f9b9baf69165521c8a578ebddc
Dhruva Veldhurthi <167943935+DVeldhurthi@users.noreply.github.com>
2025-12-28 20:23:41 -0800

    Update load_model.py

--- backend/model/load_model.py
+++ backend/model/load_model.py
@@ -1,10 +1,12 @@
 import pickle
 
+# FORCE these imports so pickle knows the classes
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 
+import os
 
-MODEL_PATH = "model/delay_model (1).pkl"
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "model/delay_model (1).pkl")
 
 with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
